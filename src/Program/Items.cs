de4a02f5e636ba3ae91f3df577e10a1692bb1384
Facundo-Farnes <facundo.farnes@correo.ucu.edu.uy>
2024-09-11 09:04:27 -0300

    First iteration of items branch

--- src/Program/Items.cs
+++ src/Program/Items.cs
@@ -2,5 +2,7 @@
 
 public class Items
 {
-    
+    private Dictionary<string, int> attackItems = new Dictionary<string, int>();
+    private Dictionary<string, int> defenseItems = new Dictionary<string, int>();
+
 }
