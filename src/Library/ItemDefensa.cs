63536cdad65b5caf65ab5a5c5eca88331d90daea
Thiago202005 <thiago.garcia@correo.ucu.edu.uy>
2024-09-22 16:42:37 -0300

    Cambios

--- src/Library/ItemDefensa.cs
+++ src/Library/ItemDefensa.cs
@@ -5,5 +5,8 @@ public class ItemDefensa : IDefensa
     public string Nombre { get; set; }
     public int Defensa { get; set; }
 
-    public int ObtenerDefensa() => Defensa;
+    public int ObtenerDefensa()
+    {
+        return Defensa;
+    }
 }
